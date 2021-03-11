d6bb64d518abc17bf68bfd1afee13eb46f6b2aa7
Andres Padilla <afpadilla84@misena.edu.co>
2021-03-11 17:45:09 -0500

    Input, Output y viewchild

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -3,14 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class BreadcrumbsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
